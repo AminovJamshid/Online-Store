b9b2d89e60cf96a07e2e1fe02a3bc07653a83e9f
AminovJamshid <aminovjamshid777@gmail.com>
2024-10-24 20:40:18 +0500

    PATCH: renamed table 2

--- database/migrations/2024_10_08_122339_create_products_table.php
+++ database/migrations/2024_10_08_122339_create_products_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->text('description');
             $table->integer('price');
             $table->unsignedBigInteger('image');
-            $table->foreign('image')->references('id')->on('images');
+//            $table->foreign('image')->references('id')->on('images');
             $table->unsignedBigInteger('category_id');
             $table->foreign('category_id')->references('id')->on('categories');
             $table->text('comment');
